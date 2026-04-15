8362cdd4e2d4d601b0880ce2f49fc4d2e9ac0a42
vikrams2333-ai <vikrams2333@gmail.com>
2026-04-15 08:46:08 +0530

    Update multiplayer-server.js

--- multiplayer-server.js
+++ multiplayer-server.js
@@ -1,7 +1,72 @@
-/**
- * Entry point kept for familiarity. The implementation lives in server/index.js
- * (used by `npm run server` on Render). Run either:
- *   node multiplayer-server.js
- *   npm run server
- */
-require("./server/index.js");
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+let waitingPlayer = null;
+
+io.on("connection", (socket) => {
+
+  console.log("Player connected:", socket.id);
+
+  socket.on("joinGame", () => {
+
+    if (waitingPlayer) {
+
+      const room = waitingPlayer.id + "#" + socket.id;
+
+      socket.join(room);
+      waitingPlayer.join(room);
+
+      socket.room = room;
+      waitingPlayer.room = room;      
+
+      socket.emit("start", { room, color: "black" });
+      waitingPlayer.emit("start", { room, color: "white" });
+
+      waitingPlayer = null;
+
+    } else {
+
+      waitingPlayer = socket;
+      socket.emit("waiting");
+
+    }
+
+  });
+
+  socket.on("move", (data) => {
+
+  console.log("MOVE FROM PLAYER:", data);
+
+  io.to(socket.room).emit("move", data.move);
+
+});
+
+  socket.on("disconnect", () => {
+
+    console.log("Player disconnected");
+
+    if (waitingPlayer === socket) {
+      waitingPlayer = null;
+    }
+
+  });
+
+});
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log("Multiplayer server running on port", PORT);
+});
