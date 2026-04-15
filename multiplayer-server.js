6752a16c5da7095f5a14ca2fdd8c96c89605e817
vikrams2333-ai <vikrams2333@gmail.com>
2026-04-15 08:32:05 +0530

    Add files via upload

---
+++ multiplayer-server.js
@@ -0,0 +1,7 @@
+/**
+ * Entry point kept for familiarity. The implementation lives in server/index.js
+ * (used by `npm run server` on Render). Run either:
+ *   node multiplayer-server.js
+ *   npm run server
+ */
+require("./server/index.js");
